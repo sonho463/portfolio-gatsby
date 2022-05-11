61f1027cf93ce815c8fb9c447f07f2326b860a4a
Son Hong Su <son.ho463@gmail.com>
2022-05-11 18:05:51 +0900

    useEffectで対応してみた

--- src/pages/index.js
+++ src/pages/index.js
@@ -19,30 +19,30 @@ const IndexPage = () => {
 			startObserve();
 		};
 	})
+	const startObserve = () => {
+		const callback = (entries, observer) => {
+			entries.forEach((entry) => {
+				if (entry.intersectionRatio === 1) {
+					cvButton.classList.add("is-hidden");
+					console.log("active");
+				} else if (!entry.isIntersecting) {
+					cvButton.classList.remove("is-hidden");
+					console.log("hidden");
+				}
+			});
+		};
 
-  const startObserve = () => {
-    const callback = (entries, observer) => {
-      entries.forEach((entry) => {
-        if (entry.intersectionRatio === 1) {
-          cvButton.classList.add("is-hidden");
-          console.log("active");
-        } else if (!entry.isIntersecting) {
-          cvButton.classList.remove("is-hidden");
-          console.log("hidden");
-        }
-      });
-    };
+		const options = {
+			threshold: [0.2, 1.0],
+		};
 
-    const options = {
-      threshold: [0.2, 1.0],
-    };
+		const observer = new IntersectionObserver(callback, options);
+		const target = document.querySelector(".js-footer__logo");
+		const cvButton = document.querySelector(".js-cv-button");
 
-    const observer = new IntersectionObserver(callback, options);
-    const target = document.querySelector(".js-footer__logo");
-    const cvButton = document.querySelector(".js-cv-button");
+		observer.observe(target);
+	};
 
-    observer.observe(target);
-  };
 
   return (
     <>
