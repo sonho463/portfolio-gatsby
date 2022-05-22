f371baeb193ff270b32f427d3d2f8a68866545fc
Son Hong Su <son.ho463@gmail.com>
2022-05-22 17:05:16 +0900

    fix Head.js

---
+++ src/components/Head.js
@@ -0,0 +1,32 @@
+import * as React from "react";
+import { Helmet } from "react-helmet";
+
+export const Head = (props) => {
+  const { title } = props;
+
+  return (
+    <Helmet>
+      <meta
+        name="description"
+        content="ウェブ制作、開発フリーランス事務所ウェブソン代表そんほんすのポートフォリオサイトです。"
+      />
+    
+
+
+      <title>{title}</title>
+      <script
+        async
+        src="https://www.googletagmanager.com/gtag/js?id=G-B88FLKC288"
+      ></script>
+      <script>
+        {`
+	window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-B88FLKC288');
+	`}
+      </script>
+    </Helmet>
+  );
+};
