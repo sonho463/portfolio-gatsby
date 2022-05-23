84fbf3407c00aa9ad93a5eb582e7c47f321191d4
Son Hong Su <son.ho463@gmail.com>
2022-05-24 08:25:04 +0900

    console.logでタグータイトルの挙動できた

--- src/pages/works.js
+++ src/pages/works.js
@@ -3,7 +3,6 @@ import { Layout } from "../components/Layout";
 import { graphql } from "gatsby";
 
 import { ImageRender } from "../components/imageRender";
-import { RenderSwiper } from "../components/renderSwiper";
 
 import { Swiper, SwiperSlide } from "swiper/react";
 import { Navigation, Pagination, Scrollbar, A11y } from "swiper";
@@ -14,22 +13,26 @@ import "swiper/css/pagination";
 import "swiper/css/scrollbar";
 
 const WorkPost = ({ data }) => {
-  const webDevSlides = [];
-  data.webDev.edges.map(({ node }) =>
-    webDevSlides.push(
-      <SwiperSlide tag="li">
-        <p>{node.works_title}</p>
-      </SwiperSlide>
-    )
-  );
+
+	data.allTags.edges.forEach(({node})=>{
+		console.log(node.name);
+		const currentTag = node.name
+		data.allWorks.edges.forEach(({node})=>{
+			if(node.works_tag.name === currentTag){
+				console.log(node.works_title);
+			}
+		})
+	})
 
   return (
     <Layout>
-<div className="p-works-archive__title">
-			<h2 className="p-works-archive__title__text">ウェブ制作</h2>
-</div>
+      <div className="content">
+        <div className="p-works-archive__title">
+          <h2 className="p-works-archive__title__text">ウェブ制作</h2>
+        </div>
+      </div>
 
-      <Swiper
+      {/* <Swiper
         className="p-work__card-outer"
         wrapperTag="ul"
         // install Swiper modules
@@ -55,82 +58,37 @@ const WorkPost = ({ data }) => {
         {data.study.edges.map(({ node }) => (
           <SwiperSlide tag="li" className="p-work__card">
             <p>{node.works_title}</p>
-            {/* <ImageRender
-              url={node.works_image.url}
-              alt={`${node.works_title}の画像`}
-              compress="auto=compress"
-              format="auto=format"
-            /> */}
+
           </SwiperSlide>
         ))}
-      </Swiper>
-      <Swiper
-        className="p-work__card-outer"
-        wrapperTag="ul"
-        // install Swiper modules
-        modules={[Navigation, Pagination, Scrollbar, A11y]}
-        spaceBetween={5}
-        slidesPerView={1}
-        // centeredSlides={true}
-        navigation
-        // pagination={{ clickable: false }}
-        // scrollbar={{ draggable: true }}
-        breakpoints={{
-          // when window width is >= 640px
-          768: {
-            slidesPerView: 1,
-          },
-          // when window width is >= 768px
-          1024: {
-            slidesPerView: 1,
-          },
-        }}
-      >
-        {webDevSlides}
-      </Swiper>
-
-
-
+      </Swiper> */}
     </Layout>
   );
 };
 
 export const query = graphql`
   query {
-    study: allMicrocmsWorksMain(
-      filter: { works_tag: { name: { eq: "勉強会" } } }
-    ) {
+    allTags: allMicrocmsWorksTag {
+      edges {
+        node {
+          name
+          slug
+          id
+        }
+      }
+    }
+    allWorks: allMicrocmsWorksMain {
       edges {
         node {
           id
           works_tag {
             name
+            slug
           }
           works_title
           works_image {
             url
           }
-        }
-      }
-    }
-    webDev: allMicrocmsWorksMain(
-      filter: { works_tag: { name: { eq: "ウェブ開発" } } }
-    ) {
-      edges {
-        node {
-          works_tag {
-            name
-          }
-          works_title
-        }
-      }
-    }
-    webCreate: allMicrocmsWorksMain(
-      filter: { works_tag: { name: { eq: "ウェブ制作" } } }
-    ) {
-      edges {
-        node {
-          works_title
         }
       }
     }
