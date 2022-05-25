d3591da5887d6cc2ec3ef3dceeaf20c90e920438
Son Hong Su <son.ho463@gmail.com>
2022-05-26 07:52:43 +0900

    Add validation for contact form

--- src/pages/contact.jsx
+++ src/pages/contact.jsx
@@ -1,10 +1,18 @@
 import * as React from "react";
 import { Layout } from "../components/Layout";
-import { useLocation } from "@reach/router";
+import { useForm } from "react-hook-form";
 
 const ContactForm = () => {
-  const location = useLocation();
-  console.log(location);
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } = useForm();
+
+	const onSubmit = data => console.log(data);
+
+
   return (
     <Layout>
       <div class="l-main">
@@ -22,15 +30,20 @@ const ContactForm = () => {
                 method="POST"
                 data-netlify="true"
                 data-netlify-honeypot="bot-field"
+								onSubmit={handleSubmit(onSubmit)}
               >
                 <input type="hidden" name="form-name" value="contact" />
                 <label for="name" class="p-contact__name">
                   おなまえ
-                  <input type="text" name="name" id="name" />
+                  <input {...register("name",{required: true})}/>
+									{errors.name?.type === 'required' && "Name is required"}
+
                 </label>
                 <label for="email" class="p-contact__email">
                   メールアドレス
-                  <input type="email" name="email" id="email" />
+                  <input {...register("email",{required: true})}/>
+									{errors.email?.type === 'required' && "Email is required"}
+
                 </label>
                 <label for="email_validate" class="p-contact__email_validate">
                   メールアドレス（確認）
@@ -38,7 +51,9 @@ const ContactForm = () => {
                     type="text"
                     name="email_validate"
                     id="email_validate"
-                  />
+										{...register("emailvalidation",{required: true})}
+										/>
+										{errors.emailvalidation?.type === 'required' && "Email is required"}
                 </label>
                 <label for="content" class="p-contact__content">
                   お問い合わせ内容
