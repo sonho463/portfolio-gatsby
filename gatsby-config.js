855454a7effe44d7f7bea8853d8238230a7a4410
Son Hong Su <son.ho463@gmail.com>
2022-05-29 10:15:57 +0900

    add Googel Analytice G-tag

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,4 +1,4 @@
-require('dotenv').config({
+require("dotenv").config({
   path: `.env.${process.env.NODE_ENV}`,
 });
 
@@ -7,10 +7,11 @@ module.exports = {
     title: "そんほんすポートフォリオ",
     url: "https://portfolio.horumont.info",
     siteUrl: "https://portfolio.horumont.info",
-		titleTemplate: "%s · The Real Hero",
-		description: "大阪・八尾のウェブ制作、開発フリーランス事務所WEBSONのそんほんすのポートフォリオサイトです。",
-		image: "/og_image.png",
-		twitterUsername: "@horumont"
+    titleTemplate: "%s · The Real Hero",
+    description:
+      "大阪・八尾のウェブ制作、開発フリーランス事務所WEBSONのそんほんすのポートフォリオサイトです。",
+    image: "/og_image.png",
+    twitterUsername: "@horumont",
   },
   plugins: [
     "gatsby-plugin-sass",
@@ -33,7 +34,7 @@ module.exports = {
       },
       __key: "images",
     },
-		{
+    {
       resolve: `gatsby-plugin-manifest`,
       options: {
         name: "WEBSONそんほんすポートフォリオ",
@@ -45,35 +46,41 @@ module.exports = {
         icon: "favicon.png",
       },
     },
-		{
-			resolve: "gatsby-plugin-typography",
-			options: {
-				pathToConfigModule: "src/utils/typography",
-			},
-		},
-		`gatsby-plugin-image`,
+    {
+      resolve: "gatsby-plugin-typography",
+      options: {
+        pathToConfigModule: "src/utils/typography",
+      },
+    },
+    `gatsby-plugin-image`,
     `gatsby-plugin-sharp`,
     `gatsby-transformer-sharp`,
-		{
-			resolve: 'gatsby-source-microcms',
+    {
+      resolve: "gatsby-source-microcms",
       options: {
         apiKey: process.env.MICROCMS_API_KEY,
         serviceId: process.env.MICROCMS_SERVICE_ID,
         apis: [
           {
-            endpoint: 'works-main',
+            endpoint: "works-main",
           },
           {
-            endpoint: 'works-tag',
+            endpoint: "works-tag",
           },
         ],
       },
-		},
-		{
-			resolve: 'gatsby-source-wordpress',
-			options: {
-				url: 'https://horumont.info/graphql',
-			}
-		},
+    },
+    {
+      resolve: "gatsby-source-wordpress",
+      options: {
+        url: "https://horumont.info/graphql",
+      },
+    },
+    {
+      resolve: `gatsby-plugin-google-gtag`,
+      options: {
+        trackingIds: [process.env.GATSBY_TRACKING_ID],
+      },
+    },
   ],
 };
